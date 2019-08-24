f681db9e900fae0df428b8a662e0e234318d4e90
Antonin Sulc <sulc.antonin@gmail.com>
2019-08-24 11:36:49 +0200

    setup scripty

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='cpt',
+      version='0.1',
+      description='A tracer for analysis of collective psychology',
+      url='https://github.com/sulcantonin/collective_psychology_tracker',
+      author='Antonin Sulc, Bastian Goldluecke',
+      author_email='sulc.antonin@gmail.com',
+      install_requires=['numpy>=1.16.2','scipy>=1.2.1','matplotlib>=2.2.4','wheel>=0.33.6','tensorflow>1.13.1','opencv-python>=1.13.1','opencv-contrib-python','PeakUtils>=1.3.2',' scikit-image>=0.14.3'])
