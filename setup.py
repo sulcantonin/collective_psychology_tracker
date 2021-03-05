b5ca70305acddb1ee335e666409ecedaf3183585
Antonin Sulc <sulc.antonin@gmail.com>
2021-03-05 15:19:58 +0900

    missing dependency (opencv-contrib-python) in setup file

--- setup.py
+++ setup.py
@@ -6,4 +6,4 @@ setup(name='cpt',
       url='https://github.com/sulcantonin/collective_psychology_tracker',
       author='Antonin Sulc, Bastian Goldluecke',
       author_email='sulc.antonin@gmail.com',
-      install_requires=['numpy>=1.16.2','scipy>=1.2.1','matplotlib>=2.2.4','wheel>=0.33.6','tensorflow>1.13.1','opencv-python>=1.13.1','opencv-contrib-python','PeakUtils>=1.3.2',' scikit-image>=0.14.3'])
+      install_requires=['numpy>=1.16.2','scipy>=1.2.1','matplotlib>=2.2.4','wheel>=0.33.6','tensorflow>1.13.1','opencv-python>=1.13.1','opencv-contrib-python','PeakUtils>=1.3.2',' scikit-image>=0.14.3','opencv-contrib-python'])
